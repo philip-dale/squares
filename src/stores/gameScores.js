6ede7a211b37901009d2164dc8f3100963cff306
Phil Dale <dalephilip@gmail.com>
2023-03-22 21:57:11 +0000

    Improve default scorboard

--- src/stores/gameScores.js
+++ src/stores/gameScores.js
@@ -1,281 +1,31 @@
 import { defineStore } from 'pinia'
+import { settingsStore } from './settings'
 
-const defaultScoreBoards = {
-    "standard": [{ "name": "", "score": 10, "time": 10000 },
-    { "name": "", "score": 9, "time": 10000 },
-    { "name": "", "score": 8, "time": 10000 },
-    { "name": "", "score": 7, "time": 10000 },
-    { "name": "", "score": 6, "time": 10000 },
-    { "name": "", "score": 5, "time": 10000 },
-    { "name": "", "score": 4, "time": 10000 },
-    { "name": "", "score": 3, "time": 10000 },
-    { "name": "", "score": 2, "time": 10000 },
-    { "name": "", "score": 1, "time": 10000 }],
-    "continuous": [{ "score": 10, "time": 10000 },
-    { "name": "", "score": 9, "time": 10000 },
-    { "name": "", "score": 8, "time": 10000 },
-    { "name": "", "score": 7, "time": 10000 },
-    { "name": "", "score": 6, "time": 10000 },
-    { "name": "", "score": 5, "time": 10000 },
-    { "name": "", "score": 4, "time": 10000 },
-    { "name": "", "score": 3, "time": 10000 },
-    { "name": "", "score": 2, "time": 10000 },
-    { "name": "", "score": 1, "time": 10000 }],
-    "oneOfEach": [{
-        "id": "1", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "2", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "3", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "4", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "5", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "6", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "7", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "8", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "9", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "10", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "11", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "12", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "13", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "14", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "15", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "16", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "17", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "18", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "19", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    ],
-    "challenge": [
-    {
-        "id": "1", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    {
-        "id": "2", "scores": [{ "name": "", "time": 10001 },
-        { "name": "", "time": 10002 },
-        { "name": "", "time": 10003 },
-        { "name": "", "time": 10004 },
-        { "name": "", "time": 10005 },
-        { "name": "", "time": 10006 },
-        { "name": "", "time": 10007 },
-        { "name": "", "time": 10008 },
-        { "name": "", "time": 10009 },
-        { "name": "", "time": 10010 }]
-    },
-    ]
+let defaultScoreBoards = {}
+
+function createDefualtScoreboard() {
+    const settings = settingsStore()
+
+    defaultScoreBoards["standard"] = [];
+    defaultScoreBoards["continuous"] = [];
+    for(let i=0; i<10; i++) {
+        defaultScoreBoards["standard"][i] = { "name": "", "score": i+1, "time": 10000 }
+        defaultScoreBoards["continuous"][i] = { "name": "", "score": i+1, "time": 10000 }
+    }
+    defaultScoreBoards["oneOfEach"] = [];
+    for(let i=0; i<Object.keys(settings.getGameLevels).length; i++) {
+        defaultScoreBoards["oneOfEach"][i] = {"id": (i+1).toString(), "scores": []}
+        for(let j=0; j<10; j++) {
+            defaultScoreBoards["oneOfEach"][i]["scores"][j] = { "name": "", "time": 10000 }
+        }
+    }
+    defaultScoreBoards["challenge"] = [];
+    for(let i=0; i<Object.keys(settings.getChallenges).length; i++) {
+        defaultScoreBoards["challenge"][i] = {"id": (i+1).toString(), "scores": []}
+        for(let j=0; j<10; j++) {
+            defaultScoreBoards["challenge"][i]["scores"][j] = { "name": "", "time": 10000 }
+        }
+    }
 }
 
 export const scoreBoardStore = defineStore('scoreBoards', {
@@ -289,6 +39,7 @@ export const scoreBoardStore = defineStore('scoreBoards', {
     },
     actions: {
         init() {
+            createDefualtScoreboard();
             let scoresState = JSON.parse(localStorage.getItem("scores_state"))
 
             if(scoresState != null){
@@ -300,16 +51,16 @@ export const scoreBoardStore = defineStore('scoreBoards', {
                         this.scoreBoards["continuous"] = scoresState.scoreBoards["continuous"]
                     }
                     if("oneOfEach" in scoresState.scoreBoards) {
-                        for (const key of Object.keys(this.scoreBoards["oneOfEach"])) {
-                            if(key in scoresState.scoreBoards["oneOfEach"]) {
-                                this.scoreBoards["oneOfEach"]["key"] = scoresState.scoreBoards["oneOfEach"]["key"]
+                        for (let i=0; i<this.scoreBoards["oneOfEach"].length; i++) {
+                            if( i+1 < scoresState.scoreBoards["oneOfEach"].length) {
+                                this.scoreBoards["oneOfEach"][i] = scoresState.scoreBoards["oneOfEach"][i]
                             }
                         }
                     }
                     if("challenge" in scoresState.scoreBoards) {
-                        for (const key of Object.keys(this.scoreBoards["challenge"])) {
-                            if(key in scoresState.scoreBoards["challenge"]) {
-                                this.scoreBoards["challenge"]["key"] = scoresState.scoreBoards["challenge"]["key"]
+                        for (let i=0; i<this.scoreBoards["challenge"].length; i++) {
+                            if( i+1 < scoresState.scoreBoards["challenge"].length) {
+                                this.scoreBoards["challenge"][i] = scoresState.scoreBoards["challenge"][i]
                             }
                         }
                     }

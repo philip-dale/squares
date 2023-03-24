010d72b936eacab1586dd9328e9b46018c98a285
Phil Dale <dalephilip@gmail.com>
2023-03-24 10:26:33 +0000

    Added welcome text

--- src/components/WelcomeScreen.vue
+++ src/components/WelcomeScreen.vue
@@ -3,7 +3,10 @@
     <div class="dialogWrapper">
       <div class="dialogContent">
         <div class="dialogTitle">Welcome To Squares Game</div>
-        <p class="instructionText">fhdsfhafoihdsafiuhafauipofhdaofiuhgdafaiuhfv dhfadoifha Dfhoiafhpo dfhafoafhoahdfosaihfdsaof</p>
+        <p class="instructionText">The purpose of the game is to create squares of a single colour by merging other squares together.</p>
+        <p class="instructionText">In Standard mode double click/tap the top box to create new samples.</p>
+        <p class="instructionText">Drag them to one of the 3 mergers in the middle on the left. When you have 3 or more in there double tap and a new square will appear on the right. Squares are made of Cells. If there is more of one colour contributing to the Cell than any other, it will be the colour of the new Cell. If there are more than one colour that are equal largest, then one is picked at random.</p>
+        <p class="instructionText">See instructions for full details and other game modes</p>
       </div>
       <div class="dialogActions">
         <v-btn class="dialogBtn" @click="$emit('playBtn')">Play</v-btn>
@@ -77,6 +80,7 @@ export default {
 
 .instructionText {
   text-align: left;
-  margin: 3px;
+  margin: 10px;
+
 }
 </style>
